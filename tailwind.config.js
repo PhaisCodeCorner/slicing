c8945947f35cfb8d60770b46d8439a504f232076
[Faizah Al Atsariyyah] <[for.collegepurposesonly158@gmail.com]>
2024-09-26 14:13:29 +0700

    slicing ka dila task

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: { fontFamily:{'papi' :["Poppins", "sans-serif"]}},
+  },
+  plugins: [],
+}
+
